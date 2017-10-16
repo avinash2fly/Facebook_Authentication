2c75846a70c0317df50d326acc41c2764b1507f8
avinash gupta <avinash2fly@gmail.com>
2017-10-16 11:11:25 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname + '/login.html'));
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
